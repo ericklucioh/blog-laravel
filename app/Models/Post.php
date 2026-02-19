d119381c4adc5bd51fb05b22177cdc06506e229c
ericklucioh <erickrealoficial@gmail.com>
2026-02-18 21:31:13 -0300

    [main] start post model

---
+++ app/Models/Post.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use App\Casts\SeoCast;
+
+/**
+ * Class Post
+ *
+ * @property string $title           Título principal do post
+ * @property string $slug            URL amigável única
+ * @property string|null $excerpt    Resumo curto do post
+ * @property bool $is_draft          Define se o post é rascunho
+ * @property string $language        Idioma do conteúdo (ex: pt-BR)
+ * @property string $category_id     UUID da categoria relacionada
+ * @property array $tags             Lista de tags do post
+ * @property \Carbon\Carbon|null $published_at Data de publicação
+ * @property \Carbon\Carbon|null $updated_at Data da última atualização
+ * @property \Carbon\Carbon|null $deleted_at Data de exclusão (soft delete)
+ * @property \App\ValueObjects\Seo|null $seo  Objeto de SEO customizado
+ * @property int $views              Total de visualizações
+ * @property int $reading_time       Tempo estimado de leitura (minutos)
+ */
+class Post extends Model
+{
+    use SoftDeletes, HasUuids;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'excerpt',
+        'is_draft',
+        'language',
+        'category_id',
+        'tags',
+        'seo',
+        'views',
+        'reading_time',
+    ];
+
+    protected $casts = [
+        'title' => 'string',
+        'slug' => 'string',
+        'excerpt' => 'string',
+        'is_draft' => 'boolean',
+        'language' => 'string',
+        'category_id' => 'string',
+        'published_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
+        'tags' => 'array',
+        'seo' => SeoCast::class,
+        'views' => 'integer',
+        'reading_time' => 'integer',
+    ];
+}
